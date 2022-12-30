8873ad057e74c0936a6956e981f0e7c794b6a84e
Margarita555 <a_rita@ukr.net>
2022-12-30 10:30:19 +0200

    refactoring

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Root } from "./Root";
 export { default as Layout } from "./Layout";
 export { default as Header } from "./Header";
 export { default as Spinner } from "./Spinner";
